f55f0d8da0a4ee9e403831c7bf6e23107096cdb1
kevinlee <phenixkeveen@gmail.com>
2022-07-30 15:55:40 +0200

    divide updated and handle error substract feature

--- substract.js
+++ substract.js
@@ -1,4 +1,8 @@
 module.exports = substract = (a, b) => {
+  const res = a - b;
+  if (Number.isNaN(res)) {
+    throw new Error("Invalid Operation");
+  }
   if (b > a) {
     throw new Error("Negative result not allowed");
   }
